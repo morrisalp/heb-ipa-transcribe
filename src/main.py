0d895ef8cf1cb6aac94707574cca2a6fbcb65dda
yakov <61390950+thewh1teagle@users.noreply.github.com>
2026-03-03 15:31:49 +0200

    refactor: enhance transcription parameters and structure

--- src/main.py
+++ src/main.py
@@ -7,15 +7,24 @@ model_id = "thewh1teagle/whisper-heb-ipa-large-v3-turbo-ct2"
 device = "cpu"  # or "cuda"
 compute_type = "int8"  # or "float16", "int8"
 language = "he"  # Hebrew language code
+min_silence_duration_ms = 500  # tune this
 
 model = WhisperModel(
     model_id,
     device=device,
     compute_type=compute_type,
-    language=language,
 )
 
-segments, info = model.transcribe("input.wav", beam_size=5)
+segments, info = model.transcribe(
+    "input.wav", 
+    beam_size=5,
+    vad_filter=True,
+    vad_parameters=dict(
+        min_silence_duration_ms=min_silence_duration_ms,  # tune this
+        threshold=0.5,
+    ),
+    language=language,
+)
 
 print("Detected language:", info.language, info.language_probability)
 
